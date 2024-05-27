ce37652778480cf006f2145916dc70f7d00f42e3
Arthur Roubado <Arthur Roubado@DESKTOP-8A7TMF7>
2024-05-27 20:27:35 -0300

    Ola mundo e teclado

---
+++ Aula01/src/Aula01/OlaMundo.java
@@ -0,0 +1,10 @@
+package Aula01;
+
+public class OlaMundo {
+ public static void main (String [] args) {
+	 
+	 System.out.println("Olá Mundo !") ;
+ }
+}
+ 
+ 
---
+++ Aula01/src/Aula01/teclado.java
@@ -0,0 +1,11 @@
+package Aula01;
+ import java.util.Scanner;
+
+public class teclado {
+ 
+	public static void main (String[] args ) {
+	 
+	Scanner scanner ** new scanner (System.in) {
+	System.out.println ("Digite seu nome aqui:");
+	String nome ** scanner Nextline ();}
+}
